bfe83eb4e1bed3cf4bca99a8a207ccf87cec4d4b
Sanidhya Pushp <pushp.sanidhya29@gmail.com>
2018-10-25 01:48:14 +0530

    Add files via upload

---
+++ LinkedList/FlattenALinkedList.cpp
@@ -0,0 +1,114 @@
+#include <bits/stdc++.h>
+struct Node{
+	int data;
+	struct Node * next;
+	struct Node * bottom ;
+};
+using namespace std;
+void printList(Node *Node)
+{
+    while (Node != NULL)
+    {
+        printf("%d ", Node->data);
+        Node = Node->bottom;
+    }
+}
+
+/*  Function which returns the  root of 
+    the flattened linked list. */
+Node *merger(Node *a, Node *b){
+    if(a==NULL){
+        return b;
+    }
+    if(b==NULL){
+        return a;
+    }
+    Node*res;
+    if(a->data<b->data){
+        res = a;
+        res->bottom = merger(a->bottom, b);
+    }
+    else{
+        res = b;
+        res->bottom = merger(a,b->bottom);
+    }
+    return res;
+}
+Node *flatten(Node *root)
+{
+   if(root==NULL || root->next==NULL){
+       return root;
+   }
+   
+   return merger(root, flatten(root->next));
+}
+
+// main function
+int main(void) {
+	int t;
+	cin>>t;
+	while(t--){
+    int n,m,flag=1,flag1=1;
+    struct Node * temp=NULL;
+	struct Node * head=NULL;
+	struct Node * pre=NULL;
+	struct Node * tempB=NULL;
+	struct Node * preB=NULL;
+		cin>>n;
+        int work[n];
+		for(int i=0;i<n;i++)
+            cin>>work[i];
+		for(int i=0;i<n;i++){
+			m=work[i];
+			--m;
+			temp = (struct Node*)malloc(sizeof(struct Node));
+			scanf("%d",&temp->data);
+			temp->next=NULL;
+			temp->bottom=NULL;
+			if(flag){
+				head=temp;
+				pre=temp;
+				flag=0;
+				flag1=1;
+			}
+			else{
+				pre->next=temp;
+				pre=temp;
+				flag1=1;
+			}
+			for(int j=0;j<m;j++){
+				tempB = (struct Node*)malloc(sizeof(struct Node));
+				scanf("%d",&tempB->data);
+				tempB->next=tempB->bottom=NULL;
+				if(flag1){
+					temp->bottom=tempB;
+					preB=tempB;
+					flag1=0;
+				}
+				else{
+					preB->bottom=tempB;
+					preB=tempB;
+				}
+			}
+		}
+		   Node *fun = head;
+		   Node *fun2=head;
+		   /*
+		while(fun!=NULL)
+            {
+                cout<<fun->data<<" ";
+                fun2 = fun->bottom;
+                fun=fun->next;
+                while(fun2){
+                cout<<fun2->data<<" ";
+                fun2=fun2->bottom;
+                }
+                cout<<endl;
+            }*/
+            Node* root = flatten(head);
+            printList(root);
+            cout<<endl;
+	}
+	return 0;
+}
+
